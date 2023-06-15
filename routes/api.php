75ef2ea39165a69768a65f370f6e638a3cdc71d3
Gaveko <garkavenko.vladislav.00@gmail.com>
2023-06-15 12:14:53 +0300

    Added films API endpoint.

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Resources\FilmCollection;
+use App\Models\Film;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,4 +19,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/films', function (Request $request) {
+    return new FilmCollection(Film::all());
 });
